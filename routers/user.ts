6f021f9e38663e51a99cb3f5106385e27bd18c55
24LP3357 24LP3357 <24LP3357@G302-CC0145.local>
2025-02-11 17:56:57 +0800

    user router

--- routers/user.ts
+++ routers/user.ts
@@ -1,25 +1,38 @@
 
-import { fetchUsers } from "../controller/user/createUser";
-import { createUser } from "../controller/user/createUser";
 import {Request, Response, Router,NextFunction } from "express";
-import { prisma } from "..";
+import { prisma } from "../";
+const findOne=async (req:Request,res:Response)=>{
+    const oneUser=await prisma.user.findOne()
+
+    res.json(oneUser)
+}
+const createUser = async (req: Request, res: Response) => {
+    const { email, password, username } = req.body;
+    try {
+      const newUser = await prisma.user.create({
+        data: {
+          email:email,
+          password: password,
+          username: username,
+        },
+      });
+      res.json(newUser);
+    } catch (e) {
+      res.send(e);
+      console.log(e);
+    }
+  };
+const fetchUsers = async (req: Request, res: Response) => {
+const users = await prisma.user.findMany({});
+res.json(users);
+};
+
 
 export const user = Router();
 
-user.get("/", fetchUsers);
-user.post("/", createUser);
-    
-user.get("/",async (req:Request,res:Response)=>{
-    const create = await prisma.user.create({
-        data: {
-            email:'sssss@gmail.com',
-            password:'akjsdhf',
-            username:'sdsdkfjsdklfj',
-        }
-    });
-    res.json(create);
-});
+user.get("/auth", fetchUsers);
+user.get("/auth:id",findOne)
+// user.patch("/auth")
+user.post("/auth", createUser);
+// user.post("/", createUser);
 
-user.post("/",async (req:Request,res:Response)=>{
-    res.json({"user":"post"});
-});
