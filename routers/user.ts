159d43cf2ed4e3ce8714c8290412c6f883925566
24LP3357 24LP3357 <24LP3357@G302-CC0145.local>
2025-02-11 13:34:08 +0800

    usercreate

--- routers/user.ts
+++ routers/user.ts
@@ -1,14 +1,12 @@
-import { profile } from "console";
+
+import { fetchUsers } from "../controller/user/createUser";
+import { createUser } from "../controller/user/createUser";
 import {Request, Response, Router,NextFunction } from "express";
 const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient()
 export const user = Router();
 
-user.get("/",async (req:Request,res:Response)=>{
-    res.json({"user":"get"});
-});
-
-user.post("/",async (req:Request,res:Response)=>{
-    res.json({"user":"post"});
-});
+user.get("/", fetchUsers);
+user.post("/", createUser);
+    
