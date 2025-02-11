a3ee4134c6ff655bbec0ef03dd894002e65841f3
Jargaltogtokh <Jagaa155@gmail.com>
2025-02-11 18:00:24 +0800

    main

---
+++ routers/profile.ts
@@ -0,0 +1,63 @@
+import { Request, Response, Router } from "express";
+import { prisma } from "..";
+
+export const profile = Router();
+
+profile.post("/", async (req: Request, res: Response) => {
+  const create = await prisma.profile.create({
+    data: req.body,
+  });
+  res.json(create);
+});
+
+profile.patch("/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const currentCard = await prisma.profile.findUnique({
+    where: { id: Number(id) },
+  });
+
+  if (!currentCard) {
+    res.status(404).json({ message: "Profile is not found" });
+  }
+
+  const update = await prisma.profile.update({
+    where: { id: Number(id) },
+    data: req.body,
+  });
+
+  res.json({ message: "Profile update is successful", update });
+});
+
+profile.delete("/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const currentCard = await prisma.profile.findUnique({
+    where: { id: Number(id) },
+  });
+
+  if (!currentCard) {
+    res.status(404).json({ message: "Profile is not found" });
+  }
+
+  const deleteprofile = await prisma.profile.delete({
+    where: { id: Number(id) },
+  });
+
+  res.json({ message: "Profile is has been removed", deleteprofile });
+});
+
+profile.get("/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const read = await prisma.profile.findMany({
+    where: {
+      id: Number(id),
+    },
+  });
+  res.json(read);
+});
+
+profile.get("/", async (req: Request, res: Response) => {
+  const read = await prisma.profile.findMany({});
+  res.json(read);
+});
--- routers/user.ts
+++ routers/user.ts
@@ -1,12 +1,14 @@
-import {Request, Response, Router,NextFunction } from "express";
+import { Request, Response, Router, NextFunction } from "express";
 import { prisma } from "..";
+import { read } from "fs";
 
 export const user = Router();
 
-user.get("/",async (req:Request,res:Response)=>{
-    res.json({"user":"get"});
+user.get("/", async (req: Request, res: Response) => {
+  const read = await prisma.user.findMany();
+  res.json(read);
 });
 
-user.post("/",async (req:Request,res:Response)=>{
-    res.json({"user":"post"});
+user.post("/", async (req: Request, res: Response) => {
+  res.json({ user: "post" });
 });
