dd252a423817211057fd4e2ae50c10ec20e60be9
24LP6831 24LP6831 <24LP6831@setups-iMac-2.local>
2025-02-13 16:52:57 +0800

    donations

--- routers/donation.ts
+++ routers/donation.ts
@@ -1,22 +1,106 @@
+import { Request, Response, Router, NextFunction } from "express";
 import { prisma } from "..";
 const express = require("express");
-const port = process.env.PORT || 8000;
-const app = express();
-app.use(express.json());
 
-app.post(
+export const donations = express();
+donations.use(express.json());
+
+donations.post(
   "/donations",
-  async (req: { body: any }, res: { json: (arg0: any) => void }) => {
-    const donation = await prisma.donation.create({ data: req.body });
-    res.json(donation);
+  async (
+    req: { body: any },
+    res: {
+      status: (arg0: number) => {
+        (): any;
+        new (): any;
+        json: { (arg0: { error: any }): void; new (): any };
+      };
+    }
+  ) => {
+    const donation = await prisma.donation.create({
+      data: req.body,
+    });
+    res.status(201).json(donation);
   }
 );
 
-app.get("/donations", async (req: any, res: { json: (arg0: any) => void }) => {
-  const donations = await prisma.donation.findMany();
-  res.json(donations);
-});
+donations.get(
+  "/donations",
+  async (
+    req: any,
+    res: {
+      status: (arg0: number) => {
+        (): any;
+        new (): any;
+        json: { (arg0: { error: any }): void; new (): any };
+      };
+    }
+  ) => {
+    const donations = await prisma.donation.findMany({
+      include: { donor: true, recipient: true },
+    });
+    res.status(200).json(donations);
+  }
+);
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+donations.get(
+  "/donations/:id",
+  async (
+    req: { params: { id: string } },
+    res: {
+      status: (arg0: number) => {
+        (): any;
+        new (): any;
+        json: { (arg0: { message?: string; error?: any }): void; new (): any };
+      };
+    }
+  ) => {
+    const donation = await prisma.donation.findUnique({
+      where: { id: parseInt(req.params.id) },
+      include: { donor: true, recipient: true },
+    });
+    donation
+      ? res.status(200).json(donation)
+      : res.status(404).json({ message: "Donation not found" });
+  }
+);
+
+donations.put(
+  "/donations/:id",
+  async (
+    req: { params: { id: string }; body: any },
+    res: {
+      status: (arg0: number) => {
+        (): any;
+        new (): any;
+        json: { (arg0: { error: string }): void; new (): any };
+      };
+    }
+  ) => {
+    const updatedDonation = await prisma.donation.update({
+      where: { id: parseInt(req.params.id) },
+      data: req.body,
+    });
+    res.status(200).json(updatedDonation);
+  }
+);
+
+donations.delete(
+  "/donations/:id",
+  async (
+    req: { params: { id: string } },
+    res: {
+      status: (arg0: number) => {
+        (): any;
+        new (): any;
+        send: { (): void; new (): any };
+        json: { (arg0: { error: string }): void; new (): any };
+      };
+    }
+  ) => {
+    await prisma.donation.delete({
+      where: { id: parseInt(req.params.id) },
+    });
+    res.status(204).send();
+  }
+);
